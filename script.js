abd44e8651d65671c60f8471bec84034f91dc8cf
ironias97 <josue_sgg@hotmail.com>
2023-07-04 07:28:35 -0600

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hi future Software Engineer!");
+function suma() {
+    var num1 = document.getElementById("number1").value;
+    var num2 = document.getElementById("number2").value;
+    document.getElementById("total").value = num1 + num2;
+}
